11570cf689956a8e50ba92511c47f7cee936a52e
Ana Xavier <ana.xavier@m2msolutions.com.br>
2019-10-31 17:39:05 -0300

    app 2 do modulo2

---
+++ src/App_old.js
@@ -0,0 +1,40 @@
+
+//import styled from 'styled-components';
+
+
+import React from 'react';
+//import styled from 'styled-components';
+import './styles/global';
+
+
+//quando n e global estilo por ocmponente
+//COMPONENT
+//const Title = styled.h1`
+ // color:#f00 ;
+  //font-size: 32px;`; 
+
+
+
+//quando n e global
+//function App() {
+ //return (
+    //<div className="App">
+    //<Fragment>  
+    //<GlobalStyle>Hello world</GlobalStyle>
+     //</Fragment> 
+    //</div>
+ // );
+//}
+
+
+/*const App = () => (
+    <Fragment>
+      <GlobalStyle />
+      <div className="App" />
+    </Fragment>
+   
+);*/
+
+//export default App;
+
+
